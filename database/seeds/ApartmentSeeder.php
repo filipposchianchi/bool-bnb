ec1ecc6c3ba4b750fcecf3bb8a5882de1ea721d0
Filippo Schianchi <schianchi.filippo.93@gmail.com>
2020-02-10 11:04:35 +0100

    fixed service error

--- database/seeds/ApartmentSeeder.php
+++ database/seeds/ApartmentSeeder.php
@@ -14,13 +14,18 @@ class ApartmentSeeder extends Seeder
      */
     public function run()
     {
-        factory(Apartment::class, 100)
+        factory(Apartment::class, 10)
             ->make()
             ->each(function($apartment) {
                 $user = User::inRandomOrder() -> first();
                 $apartment -> user() -> associate($user);
                 $apartment -> save();
                 
-            });
+            })
+
+            -> each(function($apartment){
+                $services = Service::inRandomOrder() -> take(rand(0,6)) -> get();
+                $apartment -> services() -> attach($services);
+                });
     }
 }

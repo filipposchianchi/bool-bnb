57b229a66cadb186179b849364e53f79eb9d7220
vonaxl <vonaxlcorso@gmail.com>
2020-02-10 11:22:59 +0100

    output apartment test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
 <div class="container">
-    ASDASD
+    <h1>Apartments:</h1>
+    @foreach ($apartments as $apartment)
+    <h3>
+        {{$apartment -> title}}
+    </h3>
+    <ul>
+        <li><strong>address</strong> {{$apartment -> address}}</li>
+        <li><strong>description</strong> {{$apartment -> description}}</li>
+        <li><strong>img</strong> {{$apartment -> img}}</li>
+        <li><strong>roomNum</strong> {{$apartment -> roomNum}}</li>
+        <li><strong>bedNum</strong> {{$apartment -> bedNum}}</li>
+        <li><strong>mQ</strong> {{$apartment -> mQ}}</li>
+        <li><strong>wcNum</strong> {{$apartment -> wcNum}}</li>
+        <li><strong>view</strong> {{$apartment -> view}}</li>
+    </ul>
+    @endforeach
 </div>
 @endsection

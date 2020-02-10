b7cec69c991a2dfd1a92052deb55f015c1d5895c
michele <michele.provolo@gmail.com>
2020-02-10 12:07:06 +0100

    change dest folder
    update

--- public/js/app.js
+++ public/js/app.js
@@ -49661,8 +49661,13 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
+<<<<<<< Updated upstream
 __webpack_require__(/*! C:\Users\Axl\Documents\Boolean-Back\laravel\gruppo5\bool-bnb\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! C:\Users\Axl\Documents\Boolean-Back\laravel\gruppo5\bool-bnb\resources\sass\app.scss */"./resources/sass/app.scss");
+=======
+__webpack_require__(/*! C:\Users\miche\Documents\GitHub\bool-bnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\miche\Documents\GitHub\bool-bnb\resources\sass\app.scss */"./resources/sass/app.scss");
+>>>>>>> Stashed changes
 
 
 /***/ })

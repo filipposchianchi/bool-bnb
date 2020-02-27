c34477550dc093db08919378decd4fb69c936f5a
vonaxl <vonaxlcorso@gmail.com>
2020-02-27 11:51:17 +0100

    fixed bug on homepage, minor front end changes on create-apartment page

--- public/js/app.js
+++ public/js/app.js
@@ -49367,7 +49367,7 @@ function ajaxCall(query) {
 }
 
 function addressClick() {
-  $(document).on("click", "li", function () {
+  $("form").on("click", "li", function () {
     $("#latitude").val($(this).data("lat"));
     $("#longitude").val($(this).data("lon"));
     $("#address").val($(this).text());
--- resources/views/crud/create-apartment.blade.php
+++ resources/views/crud/create-apartment.blade.php
@@ -4,75 +4,79 @@
 <div class="container py-3 ">
   <div class="row">
     {{-- form apartment registration --}}
-    <div class="col-md-6 py-5 px-3 d-flex justify-items-center align-items-center" style="border: dotted 1px gray">
-      <form action="{{route('apartment.store')}}" method="post" enctype='multipart/form-data'  class="row justify-items-between align-items-center">
+      <form action="{{route('apartment.store')}}" method="post" enctype='multipart/form-data'  class="col-md-6 p-3" style="border: 1px dotted black">
         @csrf
         @method('POST')
-        <div class="col-md-12">
-          <div class="form-group">
-            <label for="title">Nome Appartamento:</label>
-            <input class="form-control" type="text" name="title" value="" placeholder="Inserisci nome appartamento">
-          </div>
-          <div class="form-group">
-            <label for="address">Indirizzo:</label>
-            <input type="text" name="address" id="address" class="form-control input-lg" placeholder="Inserisci l'indirizzo" autocomplete="off" required/>
-            <div id="addressList">
+        <div class="row justify-items-center mt-5">
+          <h2 class="col">Creazione appartamento</h2>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="form-group">
+              <label for="title">Nome Appartamento:</label>
+              <input class="form-control" type="text" name="title" value="" placeholder="Inserisci nome appartamento">
             </div>
-            <div>
-              {{ csrf_field() }}
+            <div class="form-group">
+              <label for="address">Indirizzo:</label>
+              <input type="text" name="address" id="address" class="form-control input-lg" placeholder="Inserisci l'indirizzo" autocomplete="off" required/>
+              <div id="addressList">
+              </div>
+              <div>
+                {{ csrf_field() }}
+              </div>
+             </div>
+            <div class="form-group position">
+                <input type="text" name="latitude" id="latitude"/>
+                <input type="text" name="longitude" id="longitude"/> 
             </div>
-           </div>
-          <div class="form-group position">
-              <input type="text" name="latitude" id="latitude"/>
-              <input type="text" name="longitude" id="longitude"/> 
+            <div class="form-group">
+              <label for="description">Descrizione:</label>
+              <textarea  class="form-control" type="text" name="description" value="" placeholder="Inserisci descrizione"></textarea>
+            </div>
+            <div class="form-group">
+              <label for="image">img:</label>
+              {{-- <input class="form-control" type="text" name="img" value=""> --}}
+              <input id="image" class="form-control" type="file" name="image" value="" required>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="description">Descrizione:</label>
-            <textarea  class="form-control" type="text" name="description" value="" placeholder="Inserisci descrizione"></textarea>
+          
+          <div class="col-md-12 d-flex align-items-baseline">
+      
+            <div class="form-group col-md-3">
+              <label for="roomNum">Numero stanze:</label>
+              <input class="form-control" type="number" name="roomNum" value="1" min="0" max="20">
+            </div>
+            <div class="form-group col-md-3">
+              <label for="bedNum">Numero letti:</label>
+              <input class="form-control" type="number" name="bedNum" value="1" min="0" max="20">
+            </div>
+            <div class="form-group col-md-3">
+              <label for="mQ">Metri quadrati:</label>
+              <input class="form-control" type="number" name="mQ" value="1" min="0">
+            </div>
+            <div class="form-group col-md-3">
+              <label for="wcNum">Bagni:</label>
+              <input class="form-control" type="number" name="wcNum" value="1" min="0" max="20">
+            </div>
           </div>
-          <div class="form-group">
-            <label for="image">img:</label>
-            {{-- <input class="form-control" type="text" name="img" value=""> --}}
-            <input id="image" class="form-control" type="file" name="image" value="" required>
+      
+          <div class="col-md-12">
+            <div class="form-group">
+              <label for="services">Servizi:</label> <br>
+      
+                @foreach ($services as $service)
+                  <span class=" d-inline-block">
+                    <input name="services[]" type="checkbox"  value="{{$service->id}}"> {{$service->name}}
+                  </span> <br>
+                  @endforeach
+            </div> 
+          </div>
+          <div class="col-md-12 d-flex justify-items-center">
+            <button type="submit" class="btn btn-primary col w-75">SALVA</button>
           </div>
         </div>
         
-        <div class="col-md-12 d-flex align-items-baseline">
-    
-          <div class="form-group col-md-3">
-            <label for="roomNum">Numero stanze:</label>
-            <input class="form-control" type="number" name="roomNum" value="1" min="0" max="20">
-          </div>
-          <div class="form-group col-md-3">
-            <label for="bedNum">Numero letti:</label>
-            <input class="form-control" type="number" name="bedNum" value="1" min="0" max="20">
-          </div>
-          <div class="form-group col-md-3">
-            <label for="mQ">Metri quadrati:</label>
-            <input class="form-control" type="number" name="mQ" value="1" min="0">
-          </div>
-          <div class="form-group col-md-3">
-            <label for="wcNum">Bagni:</label>
-            <input class="form-control" type="number" name="wcNum" value="1" min="0" max="20">
-          </div>
-        </div>
-    
-        <div class="col-md-12">
-          <div class="form-group">
-            <label for="services">Servizi:</label> <br>
-    
-              @foreach ($services as $service)
-                <span class=" d-inline-block">
-                  <input name="services[]" type="checkbox"  value="{{$service->id}}"> {{$service->name}}
-                </span> <br>
-                @endforeach
-          </div> 
-        </div>
-        <div class="col-md-12 d-flex justify-items-center">
-          <button type="submit" class="btn btn-primary col w-75">SALVA</button>
-        </div>
       </form>
-    </div>
     {{-- preview output --}}
     <div class="col-md-6 p-3">
       <div class="row justify-content-center align-items-center">
